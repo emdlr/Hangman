221ec609b449bf92013f589d54f821bc85cb6f91
Edgar Martinez <e.mdlr@live.com>
2020-07-11 12:07:26 -0500

    Login Completed

--- Exec/js/init.js
+++ Exec/js/init.js
@@ -1,7 +1,23 @@
-//Initialize Login Variables 
+//Crate Classes
+class Player {
+    constructor(name,gameLevel){
+    gameLevel = gameLevel.replace(/\s+/g, '');
+    this.name=name.toUpperCase();
+    this.gameLevel=gameLevel.toUpperCase();
+    };
+};
+
+//Initialize Login Variables from Objects
 let loginLabels = document.querySelectorAll('.mainlogin label');
-let loginBtns = document.querySelectorAll('.mainlogin button');
 const loginHeader = document.querySelector('.login-title');
 const loginName = document.querySelector('.mainlogin input');
 const mainLogin = document.querySelector('.mainlogin');
 const gameLevels = document.querySelector('.level-list');
+const logExit = document.querySelector('#exit-login');
+const logStart = document.querySelector('#start-game');
+
+//Initialize Global Login Aux Varables
+let lvlSelected;
+
+
+
---
+++ Exec/js/loginscript.js
@@ -0,0 +1,78 @@
+//Naming Login Header and bittons 
+loginHeader.innerText = 'Hangman Game';
+logExit.innerText='Exit';
+logStart.innerText='Start';
+loginName.setAttribute('placeholder','Enter Player Name');
+
+//Naming Loging labels and level options
+let isFLabel=true;
+loginLabels.forEach(element => {
+    if(isFLabel){
+        element.innerHTML='</br><b>Player Name</b></br>';
+        isFLabel=false;
+    }else{
+        element.innerHTML='<b>Select Game Level</b>';
+        isFLabel=true;  
+    };
+});
+//Create Game Levels
+for(let i=0;i<3;i++){
+    let li = document.createElement('li');
+    switch (i){
+        case 0:
+            li.innerText ='H a r d';
+            break;
+        case 1:
+            li.innerText ='M e d i u m';
+            break;
+        default:
+            li.innerText ='E a s y';
+    };
+    li.setAttribute('class','newLi');
+    gameLevels.appendChild(li);
+};
+let chloginFrame = setInterval(() => {
+    if(isFLabel){
+        mainLogin.style.color='white';
+        isFLabel=false;
+    }else{
+        mainLogin.style.color = 'gray';
+        isFLabel=true;
+    };
+},1000);
+
+//Events
+logExit.addEventListener('click',exitGame);
+gameLevels.addEventListener('click',selectGameLevel);
+logStart.addEventListener('click',startGame);
+
+//Functions
+function exitGame(e){
+    e.preventDefault();
+    window.close();
+}
+function selectGameLevel(e){
+    e.preventDefault();
+    let target = e.target;
+
+    if(target.tagName === 'LI'){
+        if(lvlSelected!==undefined)
+            lvlSelected.setAttribute("class",'newLi');
+
+        lvlSelected = target;
+        lvlSelected.setAttribute('class','levelSelected');
+    };
+};
+
+function startGame(e){
+    e.preventDefault();
+    if(lvlSelected!==undefined&&loginName.value!==''){
+        let nameStr = loginName.value;
+        let glStr = lvlSelected.innerText;
+        sessionStorage.setItem('nameStr',nameStr);
+        sessionStorage.setItem('glStr',glStr);
+        window.open('index.html','_self');
+    }else
+        alert('Name or Game Level may be Missing');
+};
+
