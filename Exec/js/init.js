bd1d9173bceb0515ac3061df9f2bb5f36ca9b5fa
Edgar Martinez <e.mdlr@live.com>
2020-07-10 22:08:00 -0500

    Second Commit

---
+++ Exec/js/init.js
@@ -0,0 +1,7 @@
+//Initialize Login Variables 
+let loginLabels = document.querySelectorAll('.mainlogin label');
+let loginBtns = document.querySelectorAll('.mainlogin button');
+const loginHeader = document.querySelector('.login-title');
+const loginName = document.querySelector('.mainlogin input');
+const mainLogin = document.querySelector('.mainlogin');
+const gameLevels = document.querySelector('.level-list');
